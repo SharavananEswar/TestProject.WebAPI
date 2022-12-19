60b1426884e980ebac8f9d3d298f42756c8e6a08
Sharavanan.E <Sharavanan.E@nortonlifelock.com>
2022-12-19 20:03:59 +0530

    remove validation rule

--- ZipPay.WebApi.DTOs/CreateUserRequest.cs
+++ ZipPay.WebApi.DTOs/CreateUserRequest.cs
@@ -4,17 +4,12 @@ namespace ZipPay.WebApi.DTOs
 {
     public class CreateUserRequest
     {
-        [Required(ErrorMessage = "UserName is required")]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = "EmailAddress is required")]
-        [EmailAddress]
         public string EmailAddress { get; set; }
 
-        [Required(ErrorMessage = "MonthlySalary is required")]
         public long MonthlySalary { get; set; }
 
-        [Required(ErrorMessage = "MonthlyExpenses is required")]
         public long MonthlyExpenses { get; set; }
     }
 }
